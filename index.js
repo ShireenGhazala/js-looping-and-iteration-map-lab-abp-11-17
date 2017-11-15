7dda8914efb8595168afec6a9b57655e44fac8a8
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-15 11:16:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function lowerCaseDrivers(drivers){
 }
 
 function nameToAttributes(drivers){
- drivers.map(function(name){
+ drivers.map(function(driver){
   let fName= name.split(" ") [0];
   let lName= name.split(" ") [1];
   return {firstName:fName, lastName:lName}
